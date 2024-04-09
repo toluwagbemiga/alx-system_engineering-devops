780443702d18b29bf7cf01fd49f9b394edeb45cb
Tolulope Gbenga Olaoluwa <Tophepzz14@gmail.com>
2024-04-09 19:58:45 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -34,4 +34,12 @@ def top_ten(subreddit):
     results = response.json().get("data")
 
     # Print the titles of the top 10 hottest posts
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    [print(c.get("data").get("title")) for c in results.get("children")
+
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
