090ea47b3ffe08bb086b15ba96aa512b411c5630
Tolulope Gbenga Olaoluwa <Tophepzz14@gmail.com>
2024-04-09 19:53:27 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Module for task 0"""
-
+"""
+Script that outputs subscribers of any Reddit subreddit.
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
-        data = response.json().get("data")
-        subscribers = data.get("subscribers")
+        data = response.json()
+        subscribers = data['data']['subscribers']
         return subscribers
     else:
-        print("Error: Status code", response.status_code)
-        print("Response:", response.content)
         return 0
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-        sys.exit(1)
-
-    subreddit = sys.argv[1]
-    subscribers = number_of_subscribers(subreddit)
-    print(subscribers)
