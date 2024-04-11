a4aa78736554822dd373e6e3411a40fdb44ccb25
toluwagbemiga <tophepzz14@gmail.com>
2024-04-11 10:45:06 +0100

    recurse

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    url = f"https://www.reddit.com/r/{}/about.json"
+    headers = {"User-Agent": "My-Reddit-Scraper/1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
